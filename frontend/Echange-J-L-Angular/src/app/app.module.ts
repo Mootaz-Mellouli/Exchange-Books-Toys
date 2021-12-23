25b88444824d119629d7735b0d729b6de5e9af70
Mootaz-Mellouli <mootazmellouli@hotmail.com>
2021-12-23 15:30:02 +0100

    creating navbar

--- frontend/Echange-J-L-Angular/src/app/app.module.ts
+++ frontend/Echange-J-L-Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
