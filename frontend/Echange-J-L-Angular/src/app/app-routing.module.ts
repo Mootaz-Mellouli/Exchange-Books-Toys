e698825f2af9c4271f7ba20428305990b7401c82
Mootaz-Mellouli <mootazmellouli@hotmail.com>
2021-12-23 15:22:23 +0100

    creating users and admin module

--- frontend/Echange-J-L-Angular/src/app/app-routing.module.ts
+++ frontend/Echange-J-L-Angular/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [] ;
+const routes: Routes = [{ path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }, { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }] ;
 
 
 
