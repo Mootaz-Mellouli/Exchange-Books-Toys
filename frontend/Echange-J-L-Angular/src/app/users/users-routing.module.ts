bbfc7346d2ae21f3300f01a89beb23feced679f7
Mootaz-Mellouli <mootazmellouli@hotmail.com>
2021-12-23 21:34:49 +0100

    creating from to add toy/book

--- frontend/Echange-J-L-Angular/src/app/users/users-routing.module.ts
+++ frontend/Echange-J-L-Angular/src/app/users/users-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddFormComponent } from '../add-form/add-form.component';
 import { UsersComponent } from './users.component';
 
-const routes: Routes = [{ path: '', component: UsersComponent }];
+const routes: Routes = [
+  {path:'add',component:AddFormComponent},
+  { path: '', component: UsersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
