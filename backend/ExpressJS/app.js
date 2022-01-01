a99f61aab8aebc84d9c4e0fc8a2b550084d7bb61
Mootaz-Mellouli <mootazmellouli@hotmail.com>
2022-01-01 22:34:38 +0100

    get jouet api (working)

--- backend/ExpressJS/app.js
+++ backend/ExpressJS/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Jouet = require('./models/Jouet');
 
 mongoose.connect('mongodb://localhost:27017/exchange',
   { useNewUrlParser: true,
@@ -15,5 +16,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+  app.get('/api/jouets', (req, res) => {
+    Jouet.find()
+      .then(jouets => res.status(200).json(jouets))
+      .catch(err => res.status(400).json({error: err.message}));
+    });
 
 module.exports = app;
--- frontend/Echange-J-L-Angular/src/app/services/user.service.ts
+++ frontend/Echange-J-L-Angular/src/app/services/user.service.ts
@@ -12,7 +12,7 @@ export class UserService {
   private addBookAPI = 'http://localhost:8080/livre/add';
   private addToyAPI= 'http://localhost:8080/jouet/add';
   private toyAPI= 'http://localhost:8080/jouet';
-
+private expressapi='http://localhost:3000/api/jouets'
   private getAllToy='http://localhost:8080/jouet/';
   private getAllBook='http://localhost:8080/livre/';
   //Fake API
@@ -30,7 +30,7 @@ export class UserService {
 
   getAllToys()
   {
-    return this.http.get<Jouet[]>(this.getAllToy);
+    return this.http.get<Jouet[]>(this.expressapi);
   }
 
   getAllBooks()
