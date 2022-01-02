cc2177e0870aeeefee62c287e65f0230d2b98fb9
Malek Chtioui <chtiouimalek001@gmail.com>
2022-01-02 13:39:21 +0100

    Adding Role in Registration, backend|frontend

--- backend/Spring/exchange/src/main/java/iset/project/exchange/controller/AuthController.java
+++ backend/Spring/exchange/src/main/java/iset/project/exchange/controller/AuthController.java
@@ -1,17 +1,21 @@
 package iset.project.exchange.controller;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import iset.project.exchange.entities.Role;
 import iset.project.exchange.entities.UserAuth;
 import iset.project.exchange.repository.UserAuthRepository;
 import iset.project.exchange.services.AuthService;
@@ -25,10 +29,10 @@ public class AuthController {
 	UserAuthRepository rep;
 	
 	@PostMapping("/register")
-	public UserAuth saveUser(@RequestBody UserAuth us) {
+	public UserAuth saveUser(@RequestBody UserAuth us, @RequestParam("role") String role) {
 		String username = us.getUsername();
 		String pass = us.getPassword();
-		return auth.saveUser(username, pass);
+		return auth.saveUser(username, pass, role);
 	}
 	@GetMapping("/users")
 	public List<UserAuth> saveUser() {
@@ -38,4 +42,10 @@ public class AuthController {
 	public UserAuth getUser(@RequestParam("username") String username) {
 		return rep.findByUsername(username).get();
 	}
+	
+	@PutMapping("/users/role/{id}")
+	public UserAuth updateRole(@PathVariable("id") String id, @RequestParam("role") String role) {
+		return auth.changeRole(id, role);
+	}
+	
 }
