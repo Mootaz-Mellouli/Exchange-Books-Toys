82c587b19ce609a2ff751d1f21195202f3ce4e24
Mootaz-Mellouli <mootazmellouli@hotmail.com>
2022-01-04 00:55:55 +0100

    new project android

---
+++ mobile/ExchangeAndroid/app/src/main/java/com/example/exchangeandroid/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.exchangeandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.btnAddBook);
+    }
+    public void ajouterBook (View v)
+    {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,addBookForm.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
