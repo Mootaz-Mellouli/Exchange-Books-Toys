47da19ea4ce868b04d96e53015fe113f527b5db2
Malek Chtioui <chtiouimalek001@gmail.com>
2022-01-03 22:04:36 +0100

    Get product list

--- mobile/android/app/src/main/java/com/example/exchange_and_donate/MainActivity.java
+++ mobile/android/app/src/main/java/com/example/exchange_and_donate/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.exchange_and_donate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -32,8 +33,11 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(MainActivity.this, Products.class);
+                startActivity(i);
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
